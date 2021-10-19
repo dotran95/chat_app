e6fa58c7c5480a553e71b43d999f1e4aa73dcbe8
dotn <dotn@vnext.com.vn>
2021-10-19 18:57:05 +0700

    add sign in

---
+++ Chat/modules/SignIn/SignInView.swift
@@ -0,0 +1,20 @@
+//
+//  SignInView.swift
+//  Chat
+//
+//  Created by Do Tran on 19/10/2021.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
---
+++ Chat/modules/SignIn/SignInViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignInViewModel.swift
+//  Chat
+//
+//  Created by Do Tran on 19/10/2021.
+//
+
+import Foundation
